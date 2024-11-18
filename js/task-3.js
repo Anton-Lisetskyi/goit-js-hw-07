c6e61baa0aa53cb88b3642ac8c568fd3dadd581b
Anton L. <blizzbox88@gmail.com>
2024-11-18 14:18:21 +0200

    initials

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,11 @@
-function checkForSpam(message) {
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
 
-    message = message.toLowerCase();
-
-    if (message.includes('spam') || message.includes('sale')) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+nameInput.addEventListener('input', () => {
+    const inputValue = nameInput.value.trim();
+    if (inputValue === '') {
+    nameOutput.textContent = 'Anonymous';
+  } else {
+    nameOutput.textContent = inputValue;
+  }
+})
--- js/task-4.js
+++ js/task-4.js
@@ -1,43 +1,23 @@
-function getShippingCost(country) {
+const form = document.querySelector('.login-form');
 
-    country = country.toLowerCase();
+const onFormSubmit = event => {
+    event.preventDefault();
 
-    function getPrice(country) {
-
-        switch (country) {
-            case 'china':
-                return 100;
-                break;
-            case 'chile':
-                return 250;
-                break;
-            case 'australia':
-                return 170;
-                break;
-            case 'jamaica':
-                return 120;
-                break;
-            default:
-                return null;
-        }
-    }
-
-    const price = getPrice(country)
-
-
-    if (price !== null) {
-
-        const formattedCountry = country.charAt(0).toUpperCase() + country.slice(1);
-
-    return `Shipping to ${formattedCountry} will cost ${price} credits`;
-  } else {
-    return 'Sorry, there is no delivery to your country';
+  if (
+    form.elements.email.value.trim() === '' ||
+    form.elements.password.value.trim() === ''
+  ) {
+    alert('All form fields must be filled in');
+    return;
   }
-}
+const formData = {
+    [form.elements.email.name]: form.elements.email.value.trim(),
+    [form.elements.password.name]: form.elements.password.value.trim(),
+  };
 
-console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
-console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
-console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
-console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
-console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+  console.log(formData);
+
+  form.reset();
+};
+
+form.addEventListener('submit', onFormSubmit);
